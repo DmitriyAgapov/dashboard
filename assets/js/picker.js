3ba3ae1b170d544608e61979f88b9da5b4b81707
DmitriyAgapov <93589129+DmitriyAgapov@users.noreply.github.com>
2023-10-26 22:05:02 +0500

    without 1c styles

--- assets/js/picker.js
+++ assets/js/picker.js
@@ -8,9 +8,13 @@ document.addEventListener('DOMContentLoaded',
 				'https://cdn.jsdelivr.net/npm/@easepick/range-plugin@1.2.1/dist/index.css',
 				'https://cdn.jsdelivr.net/npm/@easepick/preset-plugin@1.2.1/dist/index.css',
 			],
+			lang: 'ru-RU',
 			zIndex: 1000,
 			plugins: ['RangePlugin', 'PresetPlugin'],
 			PresetPlugin: {
+				customLabels: ['Сегодня', 'Вчера',
+					'7 дней', '30 дней',
+					'С начала месяца', 'Последний месяц'],
 				position: 'left',
 			},
 			RangePlugin: {
